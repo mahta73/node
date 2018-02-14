7ab191741af398ddf9d8a618c36eef25894a615b
MahtaMR <zohreh.torabian7366@gmail.com>
2018-02-14 11:01:26 +0400

    more about promise

--- nodeBook/codes/shish.js
+++ nodeBook/codes/shish.js
@@ -60,3 +60,10 @@
   secondPromise.catch(error => console.log(`promise caught an error ${error.message}`));
 
   console.log(secondPromise);
+
+  /*
+
+    The Promise object is used for deferred and asynchronous computations.
+    A Promise represents an operation that hasn't completed yet but is expected in the future.
+    
+  */
