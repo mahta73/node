ced91643cbd463fdebd4ccbe6c6ec55732f0718d
MahtaMR <zohreh.torabian7366@gmail.com>
2018-02-17 15:03:43 +0400

    I am adding and commiting at the same time

--- app/calc.js
+++ app/calc.js
@@ -1,5 +1,5 @@
-  // using another branch in git 
-
+   // using another branch in git 
+	// now I am not using the master branch
   // array.reduce() method reduce our array to a single value
   let sum = function(arr) {
     return arr.reduce( (total, current) => total + current, 0 )
